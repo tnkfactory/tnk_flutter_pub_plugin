2da083079678cc4448ccc8e7bbf28841e3e6ed54
tnkfactory <tech@tnkfactory.com>
2023-08-29 16:23:30 +0900

    flutter pub plugin commit

--- test/tnk_flutter_pub_method_channel_test.dart
+++ test/tnk_flutter_pub_method_channel_test.dart
@@ -1,21 +1,21 @@
-import 'package:flutter/services.dart';
+// import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:tnk_flutter_pub/tnk_flutter_pub_method_channel.dart';
 
 void main() {
   MethodChannelTnkFlutterPub platform = MethodChannelTnkFlutterPub();
-  const MethodChannel channel = MethodChannel('tnk_flutter_pub');
+  // const MethodChannel channel = MethodChannel('tnk_flutter_pub');
 
   TestWidgetsFlutterBinding.ensureInitialized();
 
   setUp(() {
-    channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
-    });
+    // channel.setMockMethodCallHandler((MethodCall methodCall) async {
+    //   return '42';
+    // });
   });
 
   tearDown(() {
-    channel.setMockMethodCallHandler(null);
+    // channel.setMockMethodCallHandler(null);
   });
 
   test('getPlatformVersion', () async {
